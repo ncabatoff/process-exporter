8eb8e9c5f25136aa61d6f1047bb780fab0df8ad5
Daniel Palstra <daniel@danielpalstra.com>
2019-11-11 15:42:27 +0100

    Check if systemd is available

--- packaging/scripts/preremove.sh
+++ packaging/scripts/preremove.sh
@@ -1,2 +1,8 @@
-systemctl stop process-exporter.service
-systemctl disable process-exporter.service
+#!/bin/sh
+# preremove script for prometheus-process-exporter
+# Script executed after the package is removed.
+
+if [ -d /run/systemd/system ]; then
+    systemctl stop process-exporter.service
+    systemctl disable process-exporter.service
+fi
